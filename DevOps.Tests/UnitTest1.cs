ab34c7f76ff7877ba4333319150f66efc02a240f
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2021-03-03 14:25:12 +0100

    Add new operations (with tests)
    Signed-off-by: Mickael Gaillard <mickael.gaillard@tactfactory.com>

--- DevOps.Tests/UnitTest1.cs
+++ DevOps.Tests/UnitTest1.cs
@@ -19,5 +19,44 @@ namespace DevOps.Tests
 
             Assert.AreEqual(6, result);
         }
+
+        [Test]
+        public void TestSub()
+        {
+            int a = 2;
+            int b = 4;
+            Calculatrice calc = new Calculatrice();
+            int result = calc.sub(b, a);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void TestMul()
+        {
+            int a = 2;
+            int b = 4;
+            Calculatrice calc = new Calculatrice();
+            int result = calc.mul(b, a);
+
+            Assert.AreEqual(8, result);
+        }
+
+        [Test]
+        public void TestDiv()
+        {
+            int a = 2;
+            int b = 4;
+            Calculatrice calc = new Calculatrice();
+            int result = calc.div(b, a);
+
+            Assert.AreEqual(2, result);
+
+            a = 0;
+            b = 4;
+            result = calc.div(b, a);
+
+            Assert.AreEqual(0, result);
+        }
     }
 }
--- DevOps/Calculatrice.cs
+++ DevOps/Calculatrice.cs
@@ -17,5 +17,15 @@ namespace DevOps
         {
             return a - b;
         }
+
+        public int mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int div(int a, int b)
+        {
+            return a -/ b;
+        }
     }
 }
