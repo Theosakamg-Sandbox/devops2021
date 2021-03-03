4b2f7c00c5f8008248cf3d9ccf764d8c61cdddaa
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2021-03-03 12:19:29 +0100

    Add Tests
    Signed-off-by: Mickael Gaillard <mickael.gaillard@tactfactory.com>

---
+++ DevOps/Calculatrice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DevOps
+{
+    public class Calculatrice
+    {
+        public Calculatrice()
+        {
+        }
+
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
